492e9ccb4e54d24ff701563499f0ba83354e1944
Brian Bud <hello@brianbud.com>
2023-10-21 15:08:03 -0400

    divide func

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-//add
 let add = (a, b) => {
   return a + b;
 };
-//subtract
+
 let subtract = (a, b) => {
   return a - b;
 };
-//multiply
+
 let multiply = (a, b) => {
   return a * b;
 };
-//divide
 
-console.log(multiply(2, 4));
+let divide = (a, b) => {
+  return a / b;
+};
