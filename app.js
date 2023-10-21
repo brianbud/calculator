434a1377b53328596b3e1d6194bcfda80aba1f1a
Brian Bud <hello@brianbud.com>
2023-10-21 15:19:13 -0400

    create func that takes an operator and 2 nums to call the right func

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let firstNum;
+let operator;
+let secondNum;
+
 let add = (a, b) => {
   return a + b;
 };
@@ -13,3 +17,26 @@ let multiply = (a, b) => {
 let divide = (a, b) => {
   return a / b;
 };
+
+function operate(operator, num1, num2) {
+  firstNum = num1;
+  secondNum = num2;
+
+  switch (operator) {
+    case "+":
+      return add(firstNum, secondNum);
+      break;
+    case "-":
+      return subtract(firstNum, secondNum);
+      break;
+    case "*":
+      return multiply(firstNum, secondNum);
+      break;
+    case "/":
+      return divide(firstNum, secondNum);
+    default:
+      return `something wrong with the operator: ${operator}`;
+  }
+}
+
+console.log(operate("sdf", 2, 3));
